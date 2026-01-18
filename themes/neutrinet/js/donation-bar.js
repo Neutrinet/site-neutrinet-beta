99cf37899436919d159bf40f56853e1c7cdec54f
neutrinet-site <site@neutrinet.be>
2026-01-18 19:47:26 +0000

    (Grav GitSync) Automatic Commit from neutrinet-site

--- themes/neutrinet/js/donation-bar.js
+++ themes/neutrinet/js/donation-bar.js
@@ -25,7 +25,7 @@ const DONATION_TIERS = [
 ]
 
 const DONATION_GOAL = DONATION_TIERS.reduce((acc, tier) => acc + tier.amount, 0);
-const DONATION_CURRENT = 5165;
+const DONATION_CURRENT = 5195;
 
 let datasets = []
 DONATION_TIERS.forEach((tier) => {
